e22306af9d5f8d7183146d0cd5d65aee9ad919ce
kavindiyashmi <kavindiyashmi99@gmail.com>
2024-12-08 16:47:51 +0000

    index file created

---
+++ 06.12.2024/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>2020/ASP/88</h1><br><br>
+
+    <a href="getdata.php">Gettable</a><br>
+    
+    <a href="print.php">Print</a><br>
+
+
+    
+</body>
+</html>
+
